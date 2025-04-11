af11d6625d230f6322cfe43a50963014ac64d687
sumansaurabh <ss.sumansaurabh92@gmail.com>
2025-04-11 17:57:26 +0530

    feat: integrate in-house analytics for form submissions and scroll events

--- src/components/Youtube/Youtube.tsx
+++ src/components/Youtube/Youtube.tsx
@@ -1,4 +1,5 @@
 import { mp_track_video_play } from "@/lib/mixpanel";
+import { inHouseAnalytics } from "@/utils/gtag";
 import { sendGAEvent } from "@next/third-parties/google";
 import LiteYouTubeEmbed, { imgResolution } from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
@@ -13,6 +14,7 @@ export function YoutubeEmbed({ videoId, title, poster }: YoutubeEmbedType) {
   const handlePlay = () => {
     sendGAEvent("event", "video_view");
     mp_track_video_play()
+    inHouseAnalytics("video_view",{"videoId": "-1"})
   };
 
   return (
